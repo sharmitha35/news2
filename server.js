54c956033b17324d11a1c1284ee09e4dcb8bad63
sharmitha35 <88612806+sharmitha35@users.noreply.github.com>
2022-06-15 21:47:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const path = require('path');
 
 const app = express();
 
-const cors = require("cors")
-app.use(cors())
-
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/news'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/news/'}),
+    res.sendFile('index.html', {root: 'src'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080,()=>{
+    console.log("Server started at 8080")
+});
